b645ba92ddd921d48acbc99ec38013e12a26525e
Matthias Deja <MadocDoyu@gmail.com>
2018-12-09 20:14:16 +0100

    Replaced the rounding test with a specification-based test.

--- src/test/scala/net/jackadull/scurps/models/math/RoundingSpecificationTest.scala
+++ src/test/scala/net/jackadull/scurps/models/math/RoundingSpecificationTest.scala
@@ -2,10 +2,20 @@ package net.jackadull.scurps.models.math
 
 import net.jackadull.specdriven.Specification
 import net.jackadull.specdriven.bridge.scalatest.SpecificationForScalaTest
-import net.jackadull.gurps.specification.math.RoundingSpecification
+import net.jackadull.gurps.specification.math.{RoundingModel, RoundingSpecification}
+import spire.math.Rational
 
 class RoundingSpecificationTest extends SpecificationForScalaTest {
   def specifications:Seq[Specification[()⇒Unit]] = Seq(
-    new RoundingSpecification {}
+    new RoundingSpecification {
+      def roundingModel = new RoundingModel {
+        def roundCombatResult(value:Double) = Rounding.roundCombatResult(value)
+        def roundCombatResult(value:Rational) = Rounding.roundCombatResult(value)
+        def roundFeat(value:Double) = Rounding.roundFeat(value)
+        def roundFeat(value:Rational) = Rounding.roundFeat(value)
+        def roundPointCosts(value:Double) = Rounding.roundPointCost(value)
+        def roundPointCosts(value:Rational) = Rounding.roundPointCost(value)
+      }
+    }
   )
 }
--- src/test/scala/net/jackadull/scurps/models/math/RoundingTest.scala
+++
@@ -1,7 +0,0 @@
-package net.jackadull.scurps.models.math
-
-import net.jackadull.scurps.testing.HierarchySpec
-
-class RoundingTest extends HierarchySpec {
-  testHierarchy(RoundingTestGenerator(RoundingTestAccessorImpl))
-}
