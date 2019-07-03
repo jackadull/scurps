97dc17799c910002abb8ee50c4ad403e1e232178
Matthias Deja <MadocDoyu@gmail.com>
2019-07-03 18:36:45 +0200

    Merge `PGet` into `PMap`.

--- src/main/scala/net/jackadull/scurps/meta/PGet.scala
+++
@@ -1,9 +0,0 @@
-package net.jackadull.scurps.meta
-
-import scala.language.higherKinds
-
-/** Polymorphic lookup of a value type that depends on the key of type [[K]]. */
-trait PGet[K[_]] {
-  def contains(key:K[_]):Boolean
-  def get[V](key:K[V]):Option[V]
-}
--- src/main/scala/net/jackadull/scurps/meta/PMap.scala
+++ src/main/scala/net/jackadull/scurps/meta/PMap.scala
@@ -3,7 +3,9 @@ package net.jackadull.scurps.meta
 import scala.language.higherKinds
 
 /** Polymorphic map, whose value types are derived from the reference keys of type [[K]]. */
-trait PMap[K[_]] extends PGet[K] {
+trait PMap[K[_]] {
+  def contains(key:K[_]):Boolean
+  def get[V](key:K[V]):Option[V]
   def isEmpty:Boolean
   def removed(key:K[_]):PMap[K]
   def updated[V](key:K[V], value:V):PMap[K]
