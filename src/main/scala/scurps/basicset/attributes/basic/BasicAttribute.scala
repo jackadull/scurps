1d11ddc16e1553b4a3d9e5537eff311f40799084
Matthias Deja <MadocDoyu@gmail.com>
2019-08-08 22:37:11 +0200

    `BasicAttributeRule` named such because `BasicAttribute` is already the rule key.

--- src/main/scala/scurps/basicset/attributes/basic/BasicAttribute.scala
+++ src/main/scala/scurps/basicset/attributes/basic/BasicAttribute.scala
@@ -1,8 +1,9 @@
 package scurps.basicset.attributes.basic
 
+import scurps.meta.Score.IntScore
 import scurps.meta.{Rule0, RuleKey}
 
-trait BasicAttribute extends RuleKey[Rule0[Int]]
+trait BasicAttribute extends RuleKey[Rule0[IntScore]]
 object BasicAttribute {
   object Strength extends BasicAttribute
 }
--- src/main/scala/scurps/basicset/attributes/basic/BasicAttributeRule.scala
+++ src/main/scala/scurps/basicset/attributes/basic/BasicAttributeRule.scala
@@ -4,7 +4,7 @@ import scurps.meta.Derivation.{Defined, Undefined}
 import scurps.meta.Score.IntScore
 import scurps.meta.{Derivation, GameContext, Rule0}
 
-final case class BasicAttributeScoreRule(attribute:BasicAttribute) extends Rule0[IntScore] {
+final case class BasicAttributeRule(attribute:BasicAttribute) extends Rule0[IntScore] {
   override def apply()(implicit context:GameContext):Derivation[IntScore] =
     (context(FreeAttributeScore(attribute)), context(BoughtBasicAttributePoints(attribute))) match {
       case (freeD:Defined[IntScore], boughtD:Defined[IntScore]) => ??? // TODO sum
