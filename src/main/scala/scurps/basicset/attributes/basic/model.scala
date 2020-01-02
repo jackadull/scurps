4cfd446da042bdfb16cbf718f87ca1f8201310d3
Matthias Deja <MadocDoyu@gmail.com>
2020-01-02 16:18:45 +0100

    Simplified parameter list construction.

--- src/main/scala/scurps/basicset/attributes/basic/model.scala
+++ src/main/scala/scurps/basicset/attributes/basic/model.scala
@@ -1,18 +1,16 @@
 package scurps.basicset.attributes.basic
 
 import scurps.meta.algebra.ScurpsOps
-import scurps.meta.algebra.ScurpsOpsImplicits._
 import scurps.meta.context.{RuleContext, ValueKey}
 import scurps.meta.data.Score.IntScore
 import scurps.meta.data.WrapKey
-import scurps.meta.rule.Params
-import scurps.meta.rule.Params.Params0
+import scurps.meta.rule.Params.{Params0, p}
 import scurps.meta.rule.Rule.Rule0
 import scurps.meta.rule.RuleKey.RuleKey1
 
 trait BasicAttribute extends Rule0[IntScore] {
   override def apply[A[+_]](params:Params0, context:A[RuleContext])(implicit ops:ScurpsOps[A]):A[IntScore] =
-    BasicAttributeScore(Params(this.constant), context) // TODO maybe p(this.constant) instead of ParamsA(...)
+    BasicAttributeScore(p(this), context)
 }
 object BasicAttribute {
   object Strength extends BasicAttribute
--- src/main/scala/scurps/meta/rule/Params.scala
+++ src/main/scala/scurps/meta/rule/Params.scala
@@ -1,8 +1,11 @@
 package scurps.meta.rule
 
+import scurps.meta.algebra.ScurpsOps
+
 sealed trait Params extends Any
 object Params {
-  def apply[T1](p1:T1):Params1[T1] = PCons(p1, PNil)
+  @inline def p[T1](p1:T1):Params1[T1] = PCons(p1, PNil)
+  @inline def p[A[+_],T1](p1:T1)(implicit ops:ScurpsOps[A]):Params1[A[T1]] = p(ops.constant(p1))
 
   type Params0 = PNil
   type Params1[+T1] = PCons[T1,PNil]
