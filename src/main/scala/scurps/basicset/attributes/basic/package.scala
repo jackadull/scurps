523008487449961a9e3188f7a6ae8c1fb815713c
Matthias Deja <MadocDoyu@gmail.com>
2020-01-05 16:28:32 +0100

    Structural extractors for params.

--- src/main/scala/scurps/basicset/attributes/basic/package.scala
+++ src/main/scala/scurps/basicset/attributes/basic/package.scala
@@ -17,17 +17,20 @@ package object basic {
         FreeAttributeScore(params, context) :+ BoughtBasicAttributePoints(params, context) // TODO accordingTo
     },
     BoughtBasicAttributePoints -> new Rule1[BasicAttribute,IntScore] {
-      override def apply[A[+_]](params:Params1[A[BasicAttribute]], context:A[GameContext])(implicit ops:ScurpsOps[A]):A[IntScore] =
-        context.get(Subject).get(params._1).orElse(Score(0).constant) // TODO accordingTo; if there is no subject, result should be undefined
+      override def apply[A[+_]](params:Params1[A[BasicAttribute]], context:A[GameContext])(implicit ops:ScurpsOps[A]):A[IntScore] = {
+        val Params1(attribute) = params
+        context.get(Subject).get(attribute).orElse(Score(0).constant) // TODO accordingTo; if there is no subject, result should be undefined
+      }
     },
     FreeAttributeScore -> new Rule1[BasicAttribute,IntScore] {
       override def apply[A[+_]](params:Params1[A[BasicAttribute]], context:A[GameContext])(implicit ops:ScurpsOps[A]):A[IntScore] =
-        ops.constant(Score(10)) // TODO accordingTo; Score(10).constant.forAny[BasicAttribute]; undefined when no subject
+        Score(10).constant // TODO accordingTo; undefined when no subject
     },
     SetBasicAttribute -> new Rule2[BasicAttribute,IntScore,GameContext] {
       override def apply[A[+_]](params:Params2[A[BasicAttribute],A[IntScore]], context:A[GameContext])(implicit ops:ScurpsOps[A]):A[GameContext] = {
-        val (attribute, newScore) = (params._1, params._2) // TODO unapply extractor
-        context.mod(Subject) {_.updated(attribute, newScore :- FreeAttributeScore(p(attribute), context))} // TODO delete if zero; accordingTo
+        val Params2(attribute, newScore) = params
+        val newBoughtPoints = newScore :- FreeAttributeScore(p(attribute), context)
+        context.mod(Subject) {_.updated(attribute, newBoughtPoints)} // TODO delete if zero; accordingTo
       }
     }
   )
--- src/main/scala/scurps/meta/rule/Params.scala
+++ src/main/scala/scurps/meta/rule/Params.scala
@@ -10,6 +10,16 @@ object Params {
   type Params2[+T1,+T2] = PCons[T1,Params1[T2]]
   type Params3[+T1,+T2,+T3] = PCons[T1,Params2[T2,T3]]
 
+  object Params1 {
+    @inline def unapply[T1](p:Params1[T1]):Some[T1] = Some(p.head)
+  }
+  object Params2 {
+    @inline def unapply[T1,T2](p:Params2[T1,T2]):Some[(T1,T2)] = Some((p.head, p.tail.head))
+  }
+  object Params3 {
+    @inline def unapply[T1,T2,T3](p:Params3[T1,T2,T3]):Some[(T1,T2,T3)] = Some((p.head, p.tail.head, p.tail.tail.head))
+  }
+
   sealed trait PNil extends Params
   case object PNil extends PNil
 
