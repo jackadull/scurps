89f3843680255fe65bd03361fecc31d4baf89cb0
Matthias Deja <MadocDoyu@gmail.com>
2020-01-18 20:24:02 +0100

    Nice toString methods for arithmetic implicits.

--- src/main/scala/scurps/meta/data/CP.scala
+++ src/main/scala/scurps/meta/data/CP.scala
@@ -1,14 +1,26 @@
 package scurps.meta.data
 
-import scurps.meta.data.Score.IntScore
 import scurps.meta.algebra.ArithmeticOp.{Addition, IsZero, Multiplication, Subtraction}
+import scurps.meta.data.Score.IntScore
 
 /** Character points. They only come in integer values. */
 final case class CP(intValue:Int) extends AnyVal
 object CP {
-  implicit val cpAddition:Addition[CP] = {(lhs,rhs) => CP(lhs.intValue + rhs.intValue)}
-  implicit val cpIsZero:IsZero[CP] = _.intValue==0
-  implicit val cpSubtraction:Subtraction[CP] = {(lhs,rhs) => CP(lhs.intValue - rhs.intValue)}
+  implicit val cpAddition:Addition[CP] = new Addition[CP] {
+    override def apply(lhs:CP, rhs:CP):CP = CP(lhs.intValue + rhs.intValue)
+    override def toString():String = "Addition[CP]"
+  }
+  implicit val cpIsZero:IsZero[CP] = new IsZero[CP] {
+    override def apply(v:CP):Boolean = v.intValue==0
+    override def toString():String = "IsZero[CP]"
+  }
+  implicit val cpSubtraction:Subtraction[CP] = new Subtraction[CP] {
+    override def apply(lhs:CP, rhs:CP):CP = CP(lhs.intValue - rhs.intValue)
+    override def toString():String = "Subtraction[CP]"
+  }
 
-  implicit val cpWithIntScoreMultiplication:Multiplication[CP,IntScore,CP] = {(lhs,rhs) => CP(lhs.intValue * rhs.intValue)}
+  implicit val cpWithIntScoreMultiplication:Multiplication[CP,IntScore,CP] = new Multiplication[CP,IntScore,CP] {
+    override def apply(lhs:CP, rhs:IntScore):CP = CP(lhs.intValue * rhs.intValue)
+    override def toString():String = "Multiplication[CP,IntScore,CP]"
+  }
 }
--- src/main/scala/scurps/meta/data/Score.scala
+++ src/main/scala/scurps/meta/data/Score.scala
@@ -8,10 +8,22 @@ object Score {
 
   final case class IntScore private(intValue:Int) extends AnyVal with Score[Int]
   object IntScore {
-    implicit val intScoreAddition:Addition[IntScore] = {(lhs,rhs) => IntScore(lhs.intValue + rhs.intValue)}
-    implicit val intScoreIsZero:IsZero[IntScore] = _.intValue==0
-    implicit val intScoreSubtraction:Subtraction[IntScore] = {(lhs,rhs) => IntScore(lhs.intValue - rhs.intValue)}
+    implicit val intScoreAddition:Addition[IntScore] = new Addition[IntScore] {
+      override def apply(lhs:IntScore, rhs:IntScore):IntScore = IntScore(lhs.intValue + rhs.intValue)
+      override def toString():String = "Addition[IntScore]"
+    }
+    implicit val intScoreIsZero:IsZero[IntScore] = new IsZero[IntScore] {
+      override def apply(v:IntScore):Boolean = v.intValue==0
+      override def toString():String = "IsZero[IntScore]"
+    }
+    implicit val intScoreSubtraction:Subtraction[IntScore] = new Subtraction[IntScore] {
+      override def apply(lhs:IntScore, rhs:IntScore):IntScore = IntScore(lhs.intValue - rhs.intValue)
+      override def toString():String = "Subtraction[IntScore]"
+    }
 
-    implicit val intScoreWithCPMultiplication:Multiplication[IntScore,CP,CP] = {(lhs,rhs) => CP(lhs.intValue * rhs.intValue)}
+    implicit val intScoreWithCPMultiplication:Multiplication[IntScore,CP,CP] = new Multiplication[IntScore,CP,CP] {
+      override def apply(lhs:IntScore, rhs:CP):CP = CP(lhs.intValue * rhs.intValue)
+      override def toString():String = "Multiplication[IntScore,CP,CP]"
+    }
   }
 }
