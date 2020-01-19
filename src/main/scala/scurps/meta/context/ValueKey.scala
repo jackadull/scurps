5fc2cfc7e58b7d6f8cf2589a0bda0d2ff809b2b2
Matthias Deja <MadocDoyu@gmail.com>
2020-01-19 13:09:52 +0100

    Added missing todo.

--- src/main/scala/scurps/meta/context/ValueKey.scala
+++ src/main/scala/scurps/meta/context/ValueKey.scala
@@ -4,6 +4,6 @@ import scurps.meta.algebra.Optic.GetOptional
 import scurps.meta.data.PMap
 import scurps.meta.data.ShowKey.ShowKey
 
-trait ValueKey[T] extends GetOptional[PMap[ValueKey],T] with ShowKey {
+trait ValueKey[T] extends GetOptional[PMap[ValueKey],T] with ShowKey { // TODO check out toString
   override final def getOptional(source:PMap[ValueKey]):Option[T] = source.get(this)
 }
