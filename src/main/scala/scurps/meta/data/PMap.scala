8b5b39cae5fd9a3db09d688debe379a95b9f5918
Matthias Deja <MadocDoyu@gmail.com>
2020-01-19 19:57:16 +0100

    Moved `PMapLike` into the `PMap` file.

--- src/main/scala/scurps/meta/data/PMap.scala
+++ src/main/scala/scurps/meta/data/PMap.scala
@@ -33,3 +33,8 @@ object PMap {
     baseMap.toString
   }
 }
+
+trait PMapLike[K[_],+Repr<:PMapLike[K,Repr]] {
+  def removed(key:K[_]):Repr
+  def updated[V](key:K[V], value:V):Repr
+}
--- src/main/scala/scurps/meta/data/PMapLike.scala
+++
@@ -1,6 +0,0 @@
-package scurps.meta.data
-
-trait PMapLike[K[_],+Repr<:PMapLike[K,Repr]] {
-  def removed(key:K[_]):Repr
-  def updated[V](key:K[V], value:V):Repr
-}
