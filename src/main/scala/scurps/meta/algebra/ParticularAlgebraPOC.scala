3caa40de0ad995719f932060a76be6ab9eb833fd
Matthias Deja <MadocDoyu@gmail.com>
2020-02-09 13:39:53 +0100

    Looking at type relationships via traits.

--- src/main/scala/scurps/meta/algebra/ParticularAlgebraPOC.scala
+++ src/main/scala/scurps/meta/algebra/ParticularAlgebraPOC.scala
@@ -1,8 +1,18 @@
 package scurps.meta.algebra
 
-import scurps._
+import scurps.meta.algebra.ArithmeticOperation.ArithmeticOperation1
+import scurps.meta.rule.Rule
 
 // TODO remove this once POC is implemented
 object ParticularAlgebraPOC {
-  val result = 10.cp :+ 20.cp
+  //val result = 10.cp :+ 20.cp
+
+  trait ArithmeticAlgebra2[A[+_]] {
+    def arithmeticOperation[T,R](operation:ArithmeticOperation1[T,R], v:A[T]):A[R]
+  }
+
+  trait RuleRelationship[+R] {
+    type P[_[_]]
+    type Rel = Rule[P,R]
+  }
 }
