327bb3df45564d9d24a6b10ba90788c36be61431
Matthias Deja <MadocDoyu@gmail.com>
2020-02-10 00:05:26 +0100

    WIP

--- src/main/scala/scurps/package.scala
+++ src/main/scala/scurps/package.scala
@@ -1,18 +1,27 @@
+import scurps.basic_set.attributes.basic.BasicAttribute
 import scurps.meta.algebra.Arithmetic.{Addition, ArithmeticCombinator2, Subtraction}
 import scurps.meta.algebra._
+import scurps.meta.rule.Rule.Rule1
 import scurps.meta.rule.{Rule, RuleCatalog}
 import scurps.meta.unit.CPImplicits
+import scurps.meta.unit.Score.IntScore
 
 package object scurps extends CPImplicits with CollectionImplicits with OpticImplicits with ScurpsOpsImplicits {
   val basicSetRules:RuleCatalog = basic_set.basicSetRules
 
   implicit final class ArithmeticOperators[V](private val v:V) {
-    @inline def :+[AT>:V,T](v2:AT)(implicit ev:TypeContains[AT,T], combinator:ArithmeticCombinator2[AT,AT,AT,T,T,T], operation:Addition[T]):AT = // TODO check if we can leave out ev (and TypeContains)
+    // TODO simplify arithmetic combinator to [A[+_],T1,T2,R]
+    @inline def :+[AT>:V,T](v2:AT)(implicit combinator:ArithmeticCombinator2[AT,AT,AT,T,T,T], operation:Addition[T]):AT = // TODO check if we can leave out ev (and TypeContains)
       combinator.combine(operation, v, v2)
-    @inline def :-[AT>:V,T](v2:AT)(implicit ev:TypeContains[AT,T], combinator:ArithmeticCombinator2[AT,AT,AT,T,T,T], operation:Subtraction[T]):AT =
+    @inline def :-[AT>:V,T](v2:AT)(implicit combinator:ArithmeticCombinator2[AT,AT,AT,T,T,T], operation:Subtraction[T]):AT =
       combinator.combine(operation, v, v2)
   }
 
   implicit def algebraicTypeContainsItsValue[A[+_],T](implicit alg:ScurpsOps[A]):TypeContains[A[T],T] = new TypeContains[A[T],T] {} // TODO toString, singleton
   implicit def ruleContainsItsResult[P[_[_]],R]:TypeContains[Rule[P,R],R] = new TypeContains[Rule[P,R],R] {} // TODO toString, singleton
+
+  // TODO remove below
+  val r1:Rule1[BasicAttribute,IntScore] = ???
+  val r2:Rule1[BasicAttribute,IntScore] = ???
+  val r3 = r1 :+ r2
 }
